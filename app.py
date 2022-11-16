10efd85f9e6c63505db5d9f25fa39b9155f18303
Ioan Dragomir <123ioandragomir123@gmail.com>
2022-11-16 14:58:30 +0200

    Added rudimentary support for multiple authorization levels

--- app.py
+++ app.py
@@ -14,21 +14,22 @@ app.secret_key = os.environ.get('SECRET_KEY')
 CORS(app, origins = [os.environ.get('FRONTEND_ORIGIN')], supports_credentials = True)
 
 routes = {
-	'/items': {'GET': authenticated(route_get_items)},
+	'/items': {'GET': route_get_items},
 	'/item/<id>': {'GET': route_get_item},
 	'/item/<id>.jpg': {'GET': route_get_item_image},
 	
-	'/stock': {'GET': authenticated(route_get_stock)},
+	'/stock': {'GET': authenticated(route_get_stock, AuthScope.ITEMS_VIEW)},
 
-	'/tags': {'GET': authenticated(route_get_tags)},
+	'/tags': {'GET': authenticated(route_get_tags, AuthScope.TAGS_VIEW)},
 	'/tag/<id>': {'GET': route_get_tag},
-	'/tag/<id>/topUp': {'POST': route_top_up},
+	'/tag/<id>/topUp': {'POST': authenticated(route_top_up, AuthScope.TAGS_TOPUP)},
 
-	'/checkout': {'POST': authenticated(route_checkout)},
+	'/checkout': {'POST': authenticated(route_checkout, AuthScope.SELL)},
 
 	'/authenticate': {'POST': route_authenticate},
 }
 
+print('Implemented routes:')
 for route in routes:
 	for (method, handler) in routes[route].items():
 		print(method, route)
--- authentication.py
+++ authentication.py
@@ -1,31 +1,60 @@
 from flask import jsonify, session, request
 from functools import wraps
+from enum import IntFlag, auto
 
 import os
 import time
 import pyotp
-totp = pyotp.TOTP(os.environ.get('TOTP_SECRET'))
 
 from database import db_connection
 
-def authenticated(handler):
+class AuthScope(IntFlag):
+	NONE = 0
+	SELL = auto()
+	ITEMS_VIEW = auto()
+	ITEMS_EDIT = auto() # Relevant functionality unimplemented
+	TAGS_VIEW = auto()
+	TAGS_TOPUP = auto()
+	TAGS_EDIT = auto() # Relevant functionality unimplemented
+
+	SELLER = SELL | ITEMS_VIEW | TAGS_VIEW
+	MANAGER = SELL | ITEMS_VIEW | TAGS_VIEW | ITEMS_EDIT | TAGS_TOPUP | TAGS_EDIT
+
+totps = {
+	pyotp.TOTP(os.environ.get('TOTP_SELLER_SECRET')): AuthScope.SELLER,
+	pyotp.TOTP(os.environ.get('TOTP_MANAGER_SECRET')): AuthScope.MANAGER,
+}
+
+def authenticated(handler, required_scope):
 	@wraps(handler)
 	def wrapped(*args, **kwargs):
+		# Session cookie integrity is checked by flask beforehand
+		# If the session has been tampered with, we get an empty session object
+
 		if 'exp' not in session:
 			return jsonify({'error': 'Unauthenticated'})
 
 		if session['exp'] < time.time():
 			return jsonify({'error': 'Session expired, please authenticate again'})
 
-		session['exp'] = time.time() + 3600
+		if 'p' not in session:
+			return jsonify({'error': 'Unauthenticated'})
+
+		if required_scope & session['p'] != required_scope:
+			return jsonify({'error': 'Unauthorized for this action'})
+
+		session['exp'] = int(time.time() + 3600)
 
 		return handler(*args, **kwargs)
 
 	return wrapped
 
 def route_authenticate():
-	if totp.verify(request.form['totp']):
-		session['exp'] = time.time() + 3600
-		return jsonify({'message': 'Authenticated'})
+	for (totp, authscope) in totps.items():
+		if totp.verify(request.form['totp']):
+			session['exp'] = int(time.time() + 3600)
+			session['p'] = int(authscope)
+			print('Authenticated as ' + repr(authscope))
+			return jsonify({'message': 'Authenticated as ' + repr(authscope)})
 	else:
 		return jsonify({'error': 'Unauthenticated, invalid code'})
